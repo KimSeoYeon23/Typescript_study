abd4afb0803125c1f38f95438e5ee6f2038866b5
SeoYeon <sollucidus0221@naver.com>
2023-03-15 16:07:42 +0900

    README 수정

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Typescript Study
+
+```
+ts-test
+├─ index.html
+├─ package.json
+├─ README.md
+├─ src
+│  ├─ access_modifiers.ts
+│  ├─ alias.ts
+│  ├─ assertion1.ts
+│  ├─ assertion2.ts
+│  ├─ func_this.ts
+│  ├─ generic_class.ts
+│  ├─ generic_func.ts
+│  ├─ guards.ts
+│  ├─ inference.ts
+│  ├─ interface1.ts
+│  ├─ interface2.ts
+│  ├─ interface3.ts
+│  ├─ interface4.ts
+│  ├─ main.ts
+│  ├─ overloading.ts
+│  └─ type.ts
+└─ tsconfig.json
+
+```
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,7 @@
 // 제네릭(Generic)
-/// 클래스
+/// 인터엪이스 제약 조건(Constraints)
 
-class User<T> {
-  constructor(public payload: T) {};
-  getPayload() {
-    return this.payload;
-  }
+interface MyData<T> {
+  name: string;
+  value: T;
 }
-
-interface UserAType {
-  name: string;
-  age: number;
-  isValid: boolean;
-};
-
-interface UserBType {
-  name: string;
-  age: number;
-  emails: string[]
-};
-
-const heropy = new User<UserAType>({
-  name: 'Heropy',
-  age: 85,
-  isValid: true,
-});
-
-const neo = new User<UserBType>({
-  name: 'Neo',
-  age: 102,
-  emails: ['neo@gmail.com']
-});
-
-console.log(heropy);
-console.log(neo);
